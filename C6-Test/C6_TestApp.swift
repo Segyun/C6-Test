38bfe5fe2d4ec6c7266ef203cf12decfda3b912d
Huigyun Jeong <topone03@gmail.com>
2025-09-15 21:49:56 +0900

    Initial Commit

---
+++ C6-Test/C6_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  C6_TestApp.swift
+//  C6-Test
+//
+//  Created by 정희균 on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct C6_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
